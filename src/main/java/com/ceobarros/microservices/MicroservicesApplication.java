929854936b6bc99f2000d224887f490796999dad
kadu88 <carlosuntr@gmail.com>
2018-10-10 22:47:36 -0300

    Other structures

--- src/main/java/com/ceobarros/microservices/MicroservicesApplication.java
+++ src/main/java/com/ceobarros/microservices/MicroservicesApplication.java
@@ -2,24 +2,26 @@ package com.ceobarros.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-@Controller
 public class MicroservicesApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String ola(){
-		return "Olá, bem vindo ao sistema do Carlão!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesApplication.class, args);
 	}
 
-
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
+		dataSource.setUsername("root");
+		dataSource.setPassword("rootceo");
+		return dataSource;
+	}
 
 }
